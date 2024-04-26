2936b9d94d75de37787c3be24c92cb4756d1cc17
arun <arunyuvi0206@gmail.com>
2024-04-26 11:12:35 +0530

    cors header implemted for vue frontend

--- backend/settings.py
+++ backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +127,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 GRAPHENE = {
   "SCHEMA": "blog.schema.schema",
 }
+CORS_ALLOWED_ORIGINS = ("http://localhost:5173",)
