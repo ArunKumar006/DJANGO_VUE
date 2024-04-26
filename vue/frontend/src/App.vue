d1ca56b2620d60a7438d45df122e6a8975e5b568
arun <arunyuvi0206@gmail.com>
2024-04-26 22:41:20 +0530

    basic components added

--- vue/frontend/src/App.vue
+++ vue/frontend/src/App.vue
@@ -1,18 +1,16 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      <h1>My Blog 🐾</h1>
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Posts</RouterLink>
+        <RouterLink :to="{ name: 'author' }">Author</RouterLink>
+        <RouterLink :to="{ name: 'post' }">Post</RouterLink>
+        <RouterLink :to="{ name: 'tag' }">Tag</RouterLink>
       </nav>
     </div>
   </header>
@@ -21,65 +19,23 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+  h1 {
+    text-align: center;
+    font-weight: bold;
+    margin-bottom: 1rem;
+  }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
   header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    border-bottom: 1px solid #ccc;
+    margin-bottom: 1rem;
   }
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    text-align: center;
+    margin: 1rem 0;
   }
-}
+
+  nav a {
+    padding: 0.5rem;
+  }
 </style>
--- vue/frontend/src/router/index.js
+++ vue/frontend/src/router/index.js
@@ -1,23 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import AuthorView from "../views/AuthorView.vue";
+import AllPostsView from "../views/AllPostsView.vue";
+import PostView from "../views/PostView.vue";
+import PostsByTagView from "../views/PostsByTagView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "posts",
+      component: AllPostsView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/author",
+      name: "author",
+      component: AuthorView,
+    },
+    {
+      path: "/post",
+      name: "post",
+      component: PostView,
+    },
+    {
+      path: "/tag",
+      name: "tag",
+      component: PostsByTagView,
+    },
+  ],
+});
 
-export default router
+export default router;
